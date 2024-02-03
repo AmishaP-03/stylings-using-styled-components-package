d0299b8612b7224339fa9990ffc47810883e0666
Amisha PRIYA <priyaamisha03@gmail.com>
2024-02-04 00:27:51 +0530

    Dynamic and conditional styling with styled-components

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ These special components can be re-used wherever required and thus it serves the
 
 NOTE: Components created using styled-components forward all props being set on them to the underlying built-in JSX element.
 
+ADVANTAGES:
+
+1. Allows CSS styles to be re-used unlike inline styles
+2. They are still defined outside of JSX code, hence proper demarcation is maintained
+
 UI:
 
 ![image](https://github.com/AmishaP-03/stylings-using-styled-components-package/assets/154746893/7eb4b20f-6d1c-43b0-a57f-d17ba0b80431)
--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react';
 import { styled } from 'styled-components';
 
 /**
-styled is a JS object.
+ * styled is a JS object.
 
-styled.div`` --> 
-a) tagged template (a regular JS feature)
-b) a function that will create and return a div as a separate component that will have the styles we apply to it
-c) accepts template literals as input
+ * styled.div`` --> 
+ * a) tagged template (a regular JS feature)
+ * b) a function that will create and return a div as a separate component that will have the styles we apply to it
+ * c) accepts template literals as input
 
-TEMPLATE LITERAL --> contains all the styles that we want to aplly to this div
-
+ * TEMPLATE LITERAL --> contains all the styles that we want to aplly to this div
 */
 
 const ControlContainer = styled.div`
@@ -26,16 +25,23 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${({invalid}) => invalid ? '#f87171' : '#6b7280'};
 `
+/** 
+ * UP (IN COLOR PROPERTY) : 
+ * -> pass a function as value. This function will be executed by styled-components package to dynamically derive a value.
+ * -> styled-components package will give us props object as input to this anonymous function which it will execute for us.
+ * -> This props object will include all the props that are set on this styled component (here: Label).
+ * -> Thus color style is derived dynamically based on props.
+*/
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${({invalid}) => invalid ? '#fed2d2' : '#d1d5db'};
+  color: ${({invalid}) => invalid ? '#ef4444' : '#374151'};
+  border: 1px solid ${({invalid}) => invalid ? '#f73f3f' : 'transparent'};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -65,18 +71,18 @@ export default function AuthInputs() {
       {/* <div className="controls"> */}
       <ControlContainer>
         <p>
-          <Label>Email</Label>
+          <Label invalid={emailNotValid}>Email</Label>
           <Input
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            invalid={emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)} // All these props added on Input component are forwarded to the built-in JSX element
           />
         </p>
         <p>
-          <Label>Password</Label>
+          <Label invalid={passwordNotValid}>Password</Label>
           <Input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
