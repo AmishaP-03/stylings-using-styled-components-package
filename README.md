dbe566ef018b8a282dbdec0f40d2a48f96fcaa79
Amisha PRIYA <priyaamisha03@gmail.com>
2024-02-04 00:00:06 +0530

    Creating flexible components using styled-components

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ The package 'styled-components' let us create special components on which we can
 
 These special components can be re-used wherever required and thus it serves the purpose of limiting the scope of CSS styles to the components in which are defined.
 
+NOTE: Components created using styled-components forward all props being set on them to the underlying built-in JSX element.
+
 UI:
 
 ![image](https://github.com/AmishaP-03/stylings-using-styled-components-package/assets/154746893/7eb4b20f-6d1c-43b0-a57f-d17ba0b80431)
--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,4 +1,44 @@
 import { useState } from 'react';
+import { styled } from 'styled-components';
+
+/**
+styled is a JS object.
+
+styled.div`` --> 
+a) tagged template (a regular JS feature)
+b) a function that will create and return a div as a separate component that will have the styles we apply to it
+c) accepts template literals as input
+
+TEMPLATE LITERAL --> contains all the styles that we want to aplly to this div
+
+*/
+
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: #6b7280;
+`
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: #d1d5db;
+  color: #374151;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -22,18 +62,19 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      {/* <div className="controls"> */}
+      <ControlContainer>
         <p>
-          <label>Email</label>
-          <input
+          <Label>Email</Label>
+          <Input
             type="email"
             className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            onChange={(event) => handleInputChange('email', event.target.value)} // All these props added on Input component are forwarded to the built-in JSX element
           />
         </p>
         <p>
-          <label>Password</label>
-          <input
+          <Label>Password</Label>
+          <Input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
@@ -41,7 +82,8 @@ export default function AuthInputs() {
             }
           />
         </p>
-      </div>
+      </ControlContainer>
+      {/* </div> */}
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
